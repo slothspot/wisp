01974ec7d05e79959928742415348e0513d1b1b3
Austin Gibbons <austinbgibbons@gmail.com>
2014-12-05 18:19:32 -0800

    Create project files

---
+++ project/Build.scala
@@ -0,0 +1,37 @@
+import sbt._
+import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object PoppyBuild extends Build {
+
+	def sharedSettings = Defaults.defaultSettings ++ Seq(
+		version := "0.1-SNAPSHOT",
+		scalaVersion := "20.10.3",
+		organization := "com.quantifind",
+		scalacOptions := Seq("-deprecation", "-unchecked", "-optimize"),
+
+    retrieveManaged := true,
+    transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
+    resolvers ++= Seq(
+      "sonatype-snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+      "sonatype-releases" at "http://oss.sonatype.org/content/repositories/releases"
+		)
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+		)
+
+		lazy val poppy = Project("poppy", file("."), settings = poppySettings)
+
+		def poppySettings = sharedSettings ++ Seq(
+			name := "Poppy",
+			libraryDependencies ++= Seq(
+      	"net.databinder" %% "unfiltered-filter" % "0.6.7",
+      	"net.databinder" %% "unfiltered-jetty" % "0.6.7",
+      	"net.databinder" %% "unfiltered-json" % "0.6.7",
+	      "com.quantifind" %% "sumac" % "0.3.0"
+			)
+		)
+	)
+
+}
