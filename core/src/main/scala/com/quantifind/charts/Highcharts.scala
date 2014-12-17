b9db4effacaec40dec960f7b31bb5480d66c897b
Austin Gibbons <austinbgibbons@gmail.com>
2014-12-16 22:14:07 -0800

    Add a help() method to Highcharts which prints method options (hardcoded)

--- core/src/main/scala/com/quantifind/charts/Highcharts.scala
+++ core/src/main/scala/com/quantifind/charts/Highcharts.scala
@@ -2,6 +2,7 @@ package com.quantifind.charts
 
 import com.quantifind.charts.highcharts.{LeastSquareRegression, SeriesType}
 import com.quantifind.charts.repl.{HighchartsStyles, IterableIterable, IterablePair, IterablePairLowerPriorityImplicits}
+import scala.collection.immutable.ListMap
 import scala.language.implicitConversions
 
 /**
@@ -64,6 +65,33 @@ object Highcharts extends IterablePairLowerPriorityImplicits with HighchartsStyl
   def spline[A, B, C: Numeric, D: Numeric](xy: IterablePair[A, B, C, D]) = {
     val (xr, yr) = xy.toIterables
     xyToSeries(xr, yr, SeriesType.spline)
+  }
+
+  // Todo: can we disclose this information through reflection, instead of hardcoding it?
+  def help(): Unit = {
+    println("\nAvailable Plot Types: Takes an Iterable, an Iterable of pairs, a pair of Iterables, or an Iterable and a Function\n")
+    Seq("area", "areaspline", "bar", "column", "line", "pie", "scatter", "spline", "regression")
+      .map(s => "\t" + s)
+      .foreach(println)
+
+    println("\nStylistic changes:\n")
+    ListMap(
+      "hold" -> "plots the next plot on top of the existing plot",
+      "unhold" -> "plots the next plot in a new chart",
+      "title(String)" -> "add a title to the most recent plot",
+      "xAxis(String)" -> "adds a label to the x-axis",
+      "yAxis(String)" -> "adds a label to y-axis",
+      "legend(Iterable[String])" -> "adds a legend to the most recent plot",
+      """stack(["normal", "percent"])""" -> "stacks bars, columns, and lines relative to each other"
+    ).foreach{case(method, description) => println("\t%-35s%s".format(method, description))}
+
+    println("\nServer Controls:\n")
+    ListMap(
+      "undo" -> "undoes the most recent action",
+      "redo" -> "the opposite of undo",
+      "clear" -> "wipes the most recent chart from the page",
+      "clearAll" -> "wipes all plots from the page"
+    ).foreach{case(method, description) => println("\t%-35s%s".format(method, description))}
   }
 }
 
