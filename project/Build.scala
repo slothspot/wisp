799c5fe4dafa908d5116189910002426fbbfcbe0
Austin Gibbons <austinbgibbons@gmail.com>
2015-01-26 15:41:05 -0800

    Default scala version 2.11.4

--- project/Build.scala
+++ project/Build.scala
@@ -24,7 +24,7 @@ object WispBuild extends Build {
 		BranchRelease.branchSettings ++
 		xerial.sbt.Sonatype.sonatypeSettings ++
 		Seq(
-			scalaVersion := "2.10.4",
+			scalaVersion := "2.11.4",
 			crossScalaVersions := Seq("2.10.4", "2.11.4"),
 			organization := "com.quantifind",
 			scalacOptions := Seq("-deprecation", "-unchecked", "-optimize"),
